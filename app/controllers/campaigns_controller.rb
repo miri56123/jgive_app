06ef6e72f778176aec0b51416a418ef36fa6abac
Miri Bres <miribres20@gmail.com>
2026-06-17 12:48:06 +0300

    Show all donations (pending+paid) in recent donations list for consistency with progress

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,7 +1,7 @@
 class CampaignsController < ApplicationController
   def show
     @campaign = Campaign.find(params[:id])
-    @recent_donations = @campaign.donations.paid.recent.limit(20)
+    @recent_donations = @campaign.donations.recent.limit(20)
     @donation = Donation.new
   end
 end
