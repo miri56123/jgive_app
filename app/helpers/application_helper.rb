ec8f6f770bec1af9847c9774c1f3bb5e17e45ee1
Miri Bres <miribres20@gmail.com>
2026-06-17 13:07:41 +0300

    Redesign recent donations tab to match original: 3-col card grid, Hebrew timestamps, heart icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def hebrew_time_ago(time)
+    diff = (Time.current - time).to_i
+    case diff
+    when 0..59      then "עכשיו"
+    when 60..3599   then "לפני כ-#{(diff / 60)} דקות"
+    when 3600..86399 then "לפני כ-#{(diff / 3600)} שעות"
+    when 86400..2591999 then "לפני כ-#{(diff / 86400)} ימים"
+    else                  "לפני כ-#{(diff / 2592000)} חודשים"
+    end
+  end
 end
